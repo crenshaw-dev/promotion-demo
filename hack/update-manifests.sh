3d8bf318c622bec5bd43936c87cae0f94a168b46
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2023-12-19 16:02:55 -0500

    comment comments
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- hack/update-manifests.sh
+++ hack/update-manifests.sh
@@ -6,8 +6,8 @@ for env in "dev" "e2e" "prd"; do
     mkdir -p "/tmp/${app_name}"
     commit_info=$(git log -1 --pretty='format:%H %an <%ae> %s')
     manifest_file="/tmp/${app_name}/manifest.yaml"
-    echo "App: ${app_name} (https://localhost:8080/applications/${app_name})" > $manifest_file
-    echo "Commit: ${commit_info}" >> $manifest_file
+    echo "# App: ${app_name} (https://localhost:8080/applications/${app_name})" > $manifest_file
+    echo "# Commit: ${commit_info}" >> $manifest_file
     kustomize build "${env}/${region}" >> $manifest_file
   done
 done
